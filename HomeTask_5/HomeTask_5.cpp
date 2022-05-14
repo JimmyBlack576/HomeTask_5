aaeaf60421ce092309c340c76a3cccb964d5328a
Jimmy Blackwood <Jimmy Blackwood@DESKTOP-AOM0ABL>
2022-05-15 01:07:48 +0300

    Добавьте файлы проекта.

---
+++ HomeTask_5/HomeTask_5.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    float num;
+    int dollar, cent;
+
+    cout << "Введите сумму в $:\n";
+    cin >> num;
+
+    dollar = num;
+    cent = (num - dollar) * 100;
+    cout << dollar << " долларов " << cent << " центов ";
+    return 0;
+}
